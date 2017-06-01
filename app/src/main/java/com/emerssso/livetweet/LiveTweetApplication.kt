e94d576d241284ca9cae5c4631a61d162319f4c0
Conner Kasten <emerssso@gmail.com>
2017-05-31 21:39:56 -0700

    migrate to new Twitter SDK

--- app/src/main/java/com/emerssso/livetweet/LiveTweetApplication.kt
+++ app/src/main/java/com/emerssso/livetweet/LiveTweetApplication.kt
@@ -1,29 +1,13 @@
 package com.emerssso.livetweet
 
 import android.app.Application
-
-import com.twitter.sdk.android.Twitter
-import com.twitter.sdk.android.core.TwitterAuthConfig
-
-import io.fabric.sdk.android.Fabric
-
-/**
- * Created by Conner on 10/30/2016.
- */
+import com.twitter.sdk.android.core.Twitter
 
 class LiveTweetApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        val authConfig = TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET)
-        Fabric.with(this, Twitter(authConfig))
-    }
-
-    companion object {
-
-        // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-        private val TWITTER_KEY = "***REMOVED***"
-        private val TWITTER_SECRET = "***REMOVED***"
+        Twitter.initialize(this)
     }
 }

d7ff1d242934934a6bafb8c9d251a413867d7c78
Conner Kasten <emerssso@gmail.com>
2017-09-23 18:36:51 -0700

    Move API secrets to unversioned file

--- app/src/main/java/com/emerssso/livetweet/LiveTweetApplication.kt
+++ app/src/main/java/com/emerssso/livetweet/LiveTweetApplication.kt
@@ -1,13 +1,23 @@
 package com.emerssso.livetweet
 
 import android.app.Application
+import android.util.Log
+import com.twitter.sdk.android.core.DefaultLogger
 import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.TwitterConfig
 
 class LiveTweetApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        Twitter.initialize(this)
+        val config = TwitterConfig.Builder(this)
+                .logger(DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(TwitterAuthConfig(BuildConfig.TWITTER_CONSUMER_KEY,
+                        BuildConfig.TWITTER_CONSUMER_SECRET))
+                .debug(BuildConfig.DEBUG)
+                .build()
+        Twitter.initialize(config)
     }
 }
